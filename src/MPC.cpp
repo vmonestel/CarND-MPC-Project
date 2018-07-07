b3d8b474f00e35654810681bba641af260c9111a
Vianney Monestel Alfaro <vmonestel@hpe.com>
2018-07-07 17:52:52 -0600

    Fix sign of ψ Updates
    * The sign of ψ(t+1) should be negative to adjust
    to the simulator. If not then the car will turn in
    the opposite side as it is desired.

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -10,7 +10,6 @@ size_t N = 10;
 double dt = 0.1;
 
 /*
- * Both the reference cross track and orientation errors are 0.
  * The reference velocity is set to 40 mph.
  */
 double ref_v = 40;
@@ -121,12 +120,12 @@ class FG_eval {
       // epsi[t+1] = psi[t] - psides[t] + v[t] * delta[t] / Lf * dt
       fg[1 + x_start + t] = x1 - (x0 + v0 * CppAD::cos(psi0) * dt);
       fg[1 + y_start + t] = y1 - (y0 + v0 * CppAD::sin(psi0) * dt);
-      fg[1 + psi_start + t] = psi1 - (psi0 + v0 * delta0 / Lf * dt);
+      fg[1 + psi_start + t] = psi1 - (psi0 - v0 * delta0 / Lf * dt);
       fg[1 + v_start + t] = v1 - (v0 + a0 * dt);
       fg[1 + cte_start + t] =
           cte1 - ((f0 - y0) + (v0 * CppAD::sin(epsi0) * dt));
       fg[1 + epsi_start + t] =
-          epsi1 - ((psi0 - psides0) + v0 * delta0 / Lf * dt);
+          epsi1 - ((psi0 - psides0) - v0 * delta0 / Lf * dt);
     }
   }
 };
